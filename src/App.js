f959f6680af24c966c6e2da10e70a49c623eb133
SuprajaSana <sanasupraja2727@gmail.com>
2022-12-02 15:47:30 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Fragment } from "react";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { uiActions } from "./store/ui";
 
+import { uiActions } from "./store/ui";
+import { cartActions } from "./store/cart";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -12,10 +13,11 @@ import Notification from "./components/UI/Notification";
 let initialState = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
+  const changed = useSelector((state) => state.cart.changed);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -30,7 +32,10 @@ function App() {
         "https://redux-first-ac4cc-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
       if (!response.ok) {
@@ -61,9 +66,33 @@ function App() {
     });
   }, [cart, dispatch]);
 
+  useEffect(() => {
+    const fetchCartData = async () => {
+      const response = await fetch(
+        "https://redux-first-ac4cc-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      const data = await response.json();
+      dispatch(cartActions.replaceCart(data));
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+    };
+    fetchCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Fetching cart data failed",
+        })
+      );
+    });
+  }, [dispatch]);
+
   return (
     <Fragment>
-      {notification && (
+      {notification && changed && (
         <Notification
           status={notification.status}
           title={notification.title}
